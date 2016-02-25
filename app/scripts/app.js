4a81906e44d908ec22a40eeddd75a1d1594eba20
cameronwp <cameron.w.pittman@gmail.com>
2016-02-25 14:33:35 -0800

    adds better layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,7 +33,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
+    // whatevs
   });
 
   // See https://github.com/Polymer/polymer/issues/1381
